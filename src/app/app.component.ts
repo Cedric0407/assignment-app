6181350e60fd70bddbc0f0177a9dbd38b9ecbfcf
Cédric <andriamhajanirinacedric@gmail.com>
2023-05-15 10:06:49 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Welcome';
+  nom = "Ced"
+
+  getNom(){
+    return this.nom;
+  }
+}
