e62f7ed571ff82cf31e70cc47945f7d1991e0590
Cedric0407 <49508658+Cedric0407@users.noreply.github.com>
2023-06-20 19:53:35 +0300

    Update constants.ts
    update url api en local

--- src/app/shared/helpers/constants.ts
+++ src/app/shared/helpers/constants.ts
@@ -4,7 +4,7 @@ export const ROLE = {
   etudiant: "etudiant"
 }
 
-export const ENDPOINT = "https://mbdsmagascar2022-2023cedric-05tsiory-09.onrender.com/";
-// export const ENDPOINT = "http://localhost:8010/";
+// export const ENDPOINT = "https://mbdsmagascar2022-2023cedric-05tsiory-09.onrender.com/";
+export const ENDPOINT = "http://localhost:8010/";
 
 
